bbfb789210c4ac55414e868ceda8527204e5c068
Mikko Mustonen <mustonen.mikko@outlook.com>
2019-04-25 20:42:24 +0300

    Improved code

--- exercise_work_2_Mega/exercise_work_2_Mega.ino
+++ exercise_work_2_Mega/exercise_work_2_Mega.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include <Keypad.h>
 
+#define TIMEOUT_TIME 10000
+#define STATUS_MESSAGE_DELAY 2000
+
 const byte num_rows = 4;
 const byte num_cols = 4;
 char keymap[num_rows][num_cols] = {
@@ -9,40 +12,157 @@ char keymap[num_rows][num_cols] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-byte row_pins[num_rows] = {2,3,4,5};
-byte col_pins[num_cols] = {6,7,8,9};
+byte row_pins[num_rows] = {2, 3, 4, 5};
+byte col_pins[num_cols] = {6, 7, 8, 9};
 
 Keypad keypad = Keypad(makeKeymap(keymap), row_pins, col_pins, num_rows, num_cols);
 
-byte g_scan_error;
+byte scan_error;
+byte wire_address = 1;
 
 int motion_pin = 10;
 int buzzer_pin = 11;
+
+String password = "1234";
+String pass_mem = "";
+
+bool armed = true;
+bool motion_detected = false;
+
+unsigned long timeout_timer = 0;
+bool timeout = false;
+
+unsigned long last_message_time = 0;
+unsigned long status_message_sent = 0;
+
+void addressScan() {
+  int8_t address_max = 127;
+
+  for (byte scan_address = 1; scan_address < address_max; scan_address ++) {
+    Wire.beginTransmission(scan_address);
+    scan_error = Wire.endTransmission();
+    if (scan_error == 0) {
+      wire_address = scan_address;
+      Wire.beginTransmission(wire_address);
+      Wire.write("Connected");
+      scan_error = Wire.endTransmission();
+      last_message_time = millis();
+      break;
+    }
+  }
+}
+
+bool checkPass() {
+  if (password == pass_mem) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void printPass() {
+  char message[16];
+  pass_mem.toCharArray(message, 16);
+  Wire.beginTransmission(wire_address);
+  Wire.write(message);
+  scan_error = Wire.endTransmission();
+  last_message_time = millis();
+}
+
+void clearPass() {
+  pass_mem = "";
+}
+
+void backspacePass() {
+  pass_mem.remove(pass_mem.length() - 1);
+  printPass();
+}
+
+void writePass(char key) {
+  if (pass_mem.length() < password.length()) {
+    pass_mem += key;
+    printPass();
+  }
+}
 
 void setup() {
   Wire.begin();
   Serial.begin(115200);
 
-  while(!Serial)
-  {
-        
-  }
-  Serial.println("Scanning I2C...");
   pinMode(motion_pin, INPUT);
   pinMode(buzzer_pin, OUTPUT);
 }
 
 void loop() {
-  int motion_read = digitalRead(motion_pin);
-  Serial.println(motion_read);
-
-  
-  char keypressed = keypad.getKey();
-  if (keypressed != NO_KEY) {
-    Wire.beginTransmission(5);
-    Wire.write(keypressed);
-    g_scan_error = Wire.endTransmission();
+  if (scan_error != 0) {
+    addressScan();
   }
-  
-  digitalWrite(buzzer_pin, motion_read);
+
+  if (armed) {
+    motion_detected = digitalRead(motion_pin) || motion_detected;
+  }
+
+  if ((millis() - timeout_timer > TIMEOUT_TIME) && armed && !timeout) {
+    timeout = true;
+    Wire.beginTransmission(wire_address);
+    Wire.write("Timeout");
+    scan_error = Wire.endTransmission();
+  }
+
+  if (!timeout) {
+    if ((millis() - last_message_time > STATUS_MESSAGE_DELAY) && pass_mem == "" && status_message_sent <= last_message_time) {
+      Wire.beginTransmission(wire_address);
+      if (armed) {
+        Wire.write("Armed");
+      } else {
+        Wire.write("Disarmed");
+      }
+      scan_error = Wire.endTransmission();
+      status_message_sent = millis();
+    }
+
+    char keypressed = keypad.getKey();
+
+    if (!armed && keypressed != 'A') {
+      keypressed = NO_KEY;
+    }
+
+    switch (keypressed) {
+      case NO_KEY :
+        break;
+      case 'D' :
+        Wire.beginTransmission(wire_address);
+        if (checkPass()) {
+          armed = false;
+          motion_detected = false;
+          Wire.write("Right password");
+        } else {
+          Wire.write("Wrong password");
+        }
+        scan_error = Wire.endTransmission();
+        last_message_time = millis();
+
+        clearPass();
+        break;
+      case 'C' :
+        backspacePass();
+        break;
+      case 'A' :
+        armed = true;
+        status_message_sent = 0;
+        last_message_time = 0;
+        timeout_timer = millis();
+        break;
+      default :
+        writePass(keypressed);
+        break;
+    }
+  }
+
+  if (armed && motion_detected) {
+    digitalWrite(buzzer_pin, HIGH);
+  } else {
+    digitalWrite(buzzer_pin, LOW);
+    timeout_timer = millis();
+  }
 }
--- exercise_work_2_Uno/exercise_work_2_Uno.ino
+++ exercise_work_2_Uno/exercise_work_2_Uno.ino
@@ -1,86 +1,33 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(2,3,5,6,7,8);
+LiquidCrystal lcd(2, 3, 5, 6, 7, 8);
 
-const char password[] = {'1','2','3','4'};
-char pass_mem[] = {'x','x','x','x'};
-
-bool checkPass() {
-
-  for (int i = 0; i < 4; i++) {
-    if (password[i] != pass_mem[i]) {
-      Serial.println("break");
-      return false;
-    }
-    if (i == 3) {
-      Serial.println(pass_mem);
-      return true;
-    }
-  }
-  return false;
-}
-
-void printNumbers() {
-  for (int i = 0; i < 4; i++) {
-          if (pass_mem[i] == 'x') {
-            break;
-          } else {
-            lcd.print(pass_mem[i]);
-          }
-        }
-}
 
 /* If the master sends data to slave */
 void receiveEvent(int howMany) {
-  char x = Wire.read();
-  if (x == 'D') {
-    bool correct_pass = false;
-    correct_pass = checkPass();
-    for (int i = 0; i < 4; i++) {
-      pass_mem[i] = 'x';
-    }
-    
-    if (correct_pass) {
-      lcd.clear();
-      lcd.print("Password is correct");
-    } else {
-      lcd.clear();
-      lcd.print("Wrong password");
-    }
-  } else if (x == 'C') {
-    for (int i = 3; i >= 0; i--) {
-      if (pass_mem[i] != 'x') {
-        pass_mem[i] = 'x';
-        lcd.clear();
-        printNumbers();
-        break;
-      }
-    }
-  } else {
-    for (int i = 0; i < 4; i++) {
-      if (pass_mem[i] == 'x') {
-        pass_mem[i] = x;
-        lcd.clear();
-        printNumbers();
-        break;
-      }
-    }
+  String received_message = "";
+  char received_char;
+  
+  while (Wire.available()) {
+    received_char = Wire.read();
+    received_message += received_char;
   }
+  
+  lcd.clear();
+  lcd.print(received_message);
 }
 
 void setup() {
+  Serial.begin(115200);
+  Wire.begin(5);
+  Wire.onReceive(receiveEvent);
 
-    Serial.begin(115200);
-    Wire.begin(5);
-    Wire.onReceive(receiveEvent);
-
-    lcd.begin(16,2);
-    lcd.setCursor(0,0);
-    lcd.clear();
-     
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print("No connection");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(250);
 }
